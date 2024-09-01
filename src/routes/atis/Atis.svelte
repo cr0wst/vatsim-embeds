fef54ae35429452e69707f33896e2f5841d53dcf
Steve Crow <steve@smcrow.com>
2024-09-01 15:51:37 -0400

    Fix styling

--- src/routes/atis/Atis.svelte
+++ src/routes/atis/Atis.svelte
@@ -7,15 +7,15 @@
 	};
 </script>
 
-<div class="items-center justify-center flex text-xs m-2 border border-slate-500 bg-slate-800">
+<div class="items-center flex text-xs m-2 border border-slate-500 bg-slate-900">
 	<div
-		class="w-fit p-2 flex flex-col justify-center items-center border-r border-r-slate-600 bg-slate-900"
+		class="w-fit min-h-24 min-w-32 p-2 flex flex-col justify-center items-center border-r border-r-slate-600 bg-slate-900"
 	>
 		<p class="text-sm font-mono text-white">{atis.callsign}</p>
 		<p class="text-4xl font-bold text-slate-500">{atis.code}</p>
 		<p class="text-sm font-mono text-white">{atis.frequency}</p>
 	</div>
-	<div class="w-fit p-2 text-white font-mono text-justify h-full">
+	<div class="min-h-24 p-2 bg-slate-700 m-1 text-white font-mono text-justify h-full">
 		{atis.text}
 	</div>
 </div>
